0f9d968a94e90d971c200674d78e0d35f2507201
Adrian <adrian.e.rosales@gmail.com>
2019-09-19 11:19:05 -0700

    added api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://cryptic-woodland-34056.herokuapp.com' ||'http://localhost:8000';
 
     
