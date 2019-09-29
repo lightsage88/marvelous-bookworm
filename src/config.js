dcbc51ea254d71ba017bc3cdd1e5a5a3c4d09b8c
Adrian <adrian.e.rosales@gmail.com>
2019-09-29 16:43:54 -0700

    :wq `

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://cryptic-woodland-34056.herokuapp.com';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://cryptic-woodland-34056.herokuapp.com';
 
 
     
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
